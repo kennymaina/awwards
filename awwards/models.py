ef52702d839e21f15529284dcc1662ff04984cf7
kennymaina <victormurigi@outlook.com>
2019-05-30 06:36:29 +0300

    introduction of class project which contains credentials

--- awwards/models.py
+++ awwards/models.py
@@ -13,19 +13,52 @@ class Project(models.Model):
     url = models.CharField(max_length=50, blank=True)
     owner = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
  
+    def save_project(self):
+        self.save()
+
+
+    def delete_image(self):
+        self.delete()
+
     def __str__(self):
-        return self.url
+        return self.project_name
+
+    @classmethod
+    def get_by_id(cls, id):
+        details = Project.objects.filter(owner=id)
+        return details
     
     @classmethod
+    def get_all_projects(cls):
+        project = Project.objects.all()
+        return project
+
+    @classmethod
     def search_by_project(cls, search_term):
-        project = Project.objects.filter(title__icontains=search_term)
+        project = Project.objects.filter(project_name__icontains=search_term)
         return project
 
 
 class Profile(models.Model):
-    name = models.CharField(max_length =30)
-# class Meta:
-#         ordering = ['-pk']
+    profile_pic = models.ImageField(upload_to = 'profile/',blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    email = models.EmailField(null = True)
+    full_name = models.CharField(max_length=255, null=True)
+    project = models.ForeignKey(Project,null=True)
+
+    def __str__(self):
+        return self.user.username
 
     def save_profile(self):
         self.save()
+
+    @classmethod
+    def get_by_id(cls, id):
+        details = Profile.objects.get(user=id)
+        return details
+
+    @classmethod
+    def filter_by_id(cls, id):
+        details = Profile.objects.filter(user=id).first()
+        return details
+
