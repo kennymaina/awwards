a44b0882b198f092dbec7cb4e7bb3238bc36dd1e
kennymaina <victormurigi@outlook.com>
2019-05-30 07:03:15 +0300

    project testing

--- awwards/tests.py
+++ awwards/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+
+class ProjectsTestCase(TestCase):
+    def setUp(self, 'project_photo= 'bgimage.jpg', 'description='project description','url='site url','github_repo ='git repo','owner='kennymaina','project_name='awwards'):
+        return Projects.objects.create('project_photo', 'description','url','github_repo','owner','project_name')
+
+    def projectSave(self):
+        initialization = self.setUp()
+        self.assertTrue(save > 0)
