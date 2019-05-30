acd2ed4e68eebd5dfeeb9e2a989510ed59e2d2d1
kennymaina <victormurigi@outlook.com>
2019-05-30 12:37:02 +0300

    removing git repo

--- awwards/models.py
+++ awwards/models.py
@@ -9,7 +9,7 @@ class Project(models.Model):
     project_name = models.CharField(max_length=50, blank=True)
     project_photo = models.ImageField(upload_to='projectpics/')
     description = models.TextField(max_length=600, blank=True)
-    github_repo = models.CharField(max_length=200, blank=True)
+    # github_repo = models.CharField(max_length=200, blank=True)
     url = models.CharField(max_length=50, blank=True)
     owner = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
  
